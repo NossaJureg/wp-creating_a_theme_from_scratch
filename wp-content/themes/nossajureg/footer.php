4a49d08cb42f99fb8a7fa91aa171e05a8643e375
João Vítor Dias <joaodias@noctus.org>
2016-11-23 02:24:41 -0200

    Convert the index.html to index.php
    - header.php
    - footer.php

---
+++ wp-content/themes/nossajureg/footer.php
@@ -0,0 +1,23 @@
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="<?php echo WP_THEME_URL; ?>/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo WP_THEME_URL; ?>/js/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
