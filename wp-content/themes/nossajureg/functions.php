01e30d0c9a9c6d70e5c9f389293854968e40b927
João Vítor Dias <joaodias@noctus.org>
2016-11-23 02:23:49 -0200

    functions.php
    - constants
    - remove admin bar

---
+++ wp-content/themes/nossajureg/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+# CONSTANTS
+
+define('WP_URL', get_home_url());
+define('WP_THEME_URL', get_bloginfo('template_url'));
+define('WP_SITE_NAME', get_bloginfo('name'));
+
+# FUNCTIONS
+
+add_action('after_setup_theme', 'custom_setup');
+
+function custom_setup()
+{
+    // Remove admin bar
+    add_filter('show_admin_bar', '__return_false');
+}
